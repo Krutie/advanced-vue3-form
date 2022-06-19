e8d259d45540549ccfd27daff347721aa7b530be
Krutie Patel <krutie.patel@gmail.com>
2022-06-20 00:08:58 +1000

    Update props to add form config

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-    <div id="app">
-        <div class="title">
-            <h1>Advanced Vue 3 Form</h1>
-        </div>
-        <FormConfigProvider />
+  <div id="app">
+    <div class="title">
+      <h1>Advanced <span> Vue 3 </span> Form</h1>
     </div>
+    <FormConfigProvider :formConfig="formConfig" />
+  </div>
 </template>
 
 <script setup>
 import FormConfigProvider from "./components/FormConfigProvider";
+import { form as formConfig } from "./config/config";
 </script>
 <style>
 @import "./assets/styles/main.css";

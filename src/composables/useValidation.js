9209ec59afa37301c24a186f2afc172baa1abfc4
Krutie Patel <krutie.patel@gmail.com>
2022-06-20 00:09:41 +1000

    ESLint cleanup

--- src/composables/useValidation.js
+++ src/composables/useValidation.js
@@ -1,36 +1,53 @@
-import { computed } from 'vue'
-import { minLength, required, email, numeric, helpers } from '@vuelidate/validators'
+import { computed } from "vue";
+import {
+  minLength,
+  required,
+  email,
+  numeric,
+  helpers,
+} from "@vuelidate/validators";
 
 export function useValidation(props) {
-    /**
-     * values available in props
-     * props.type
-     * props.validation.minLength
-     * props.validation.required
-     * props.name
-     */
+  /**
+   * values available in props
+   * props.type
+   * props.validation.minLength
+   * props.validation.required
+   * props.name
+   */
 
-    const validationMessage = computed(() => {
-        if (props.type === "check") {
-            return `Minimum ${props.validation.minLength} ${props.name} required.`
-        }
-        return `Minimum ${props.validation.minLength} characters required for ${props.name}.`
-    })
-    const rules = computed(() => ({
-        /**
-         * Can also have custom properties attached with validation 
-         * answer: helpers.withParams({ 'key': props.name }, (value) => value),
-        */
-        selectedValue: {
-            required: props.validation.required ? helpers.withMessage(`${props.name} cannot be empty.`, required) : false,
-            minLength: props.validation.minLength ? helpers.withMessage(validationMessage.value, minLength(props.validation.minLength)) : false,
-            email: props.validation.email ? helpers.withMessage('Must be a valid email address.', email) : false,
-            numeric: props.validation.numeric ? helpers.withMessage('Must use all numbers.', numeric) : false,
-            $autoDirty: true,
-        },
-    }))
-
-    return {
-        rules
+  const validationMessage = computed(() => {
+    if (props.type === "check") {
+      return `Minimum ${props.validation.minLength} ${props.name} required.`;
     }
+    return `Minimum ${props.validation.minLength} characters required for ${props.name}.`;
+  });
+  const rules = computed(() => ({
+    /**
+     * Can also have custom properties attached with validation
+     * answer: helpers.withParams({ 'key': props.name }, (value) => value),
+     */
+    selectedValue: {
+      required: props.validation.required
+        ? helpers.withMessage(`${props.name} cannot be empty.`, required)
+        : false,
+      minLength: props.validation.minLength
+        ? helpers.withMessage(
+            validationMessage.value,
+            minLength(props.validation.minLength)
+          )
+        : false,
+      email: props.validation.email
+        ? helpers.withMessage("Must be a valid email address.", email)
+        : false,
+      numeric: props.validation.numeric
+        ? helpers.withMessage("Must use all numbers.", numeric)
+        : false,
+      $autoDirty: true,
+    },
+  }));
+
+  return {
+    rules,
+  };
 }

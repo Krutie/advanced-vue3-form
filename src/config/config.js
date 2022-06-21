e872eca76454d2130f64a6b3de8407425c9e13a5
Krutie Patel <krutie.patel@gmail.com>
2022-06-21 18:40:01 +1000

    Add html instructions with icons in form config

--- src/config/config.js
+++ src/config/config.js
@@ -1,127 +1,152 @@
+import { instructions } from "./constants";
+
 export const form = [
   {
-    "type": "information",
-    "label": "",
-    "name": "info",
-    "text": `We would like to know more about you and your current <strong>situation</strong>.`,
-    "buttonText": "Okay, cool!",
-  },
-  {
-    "type": "radio",
-    "label": "Study Option",
-    "name": "studyOption",
-    "buttonText": "What's on?",
-    "options": {
-      "choices": [
-        "Full-time",
-        "Part-time",
-        "Distant"
-      ]
+    type: "information",
+    label: "",
+    name: "info",
+    text: instructions,
+    buttonText: "Apply here",
+    options: {
+      attrs: {},
     },
-    "validation": {
-      required: true,
-    }
-  },
-  {
-    "type": "checkbox",
-    "label": "Courses",
-    "name": "courses",
-    "buttonText": "Okay, cool!",
-    "options": {
-      "choices": [
-        "Project Management",
-        "Business Analysis",
-        "Product Management"
-      ]
-    },
-    "validation": {
-      required: true,
-      minLength: 2
-    }
-  },
-  {
-    "type": "text",
-    "label": "First Name",
-    "name": "firstName",
-    "buttonText": "Next...",
-    "options": {
-      "attrs": {
-        "placeholder": "First Name"
-      }
-    },
-    "validation": {
-      required: true,
-      minLength: 5
-    }
-  },
-  {
-    "type": "text",
-    "label": "Last Name",
-    "name": "lastName",
-    "buttonText": "What do we call you?",
-    "options": {
-      "attrs": {
-        "placeholder": "Last Name"
-      }
-    },
-    "validation": {
+    validation: {
       required: false,
-      minLength: 5
-    }
-  },
-  {
-    "type": "information",
-    "label": "",
-    "name": "info",
-    "text": `Now, tell us how do we get in touch with you.`,
-    "buttonText": "Okay, cool!",
-  },
-  {
-    "type": "text",
-    "label": "Your Email",
-    "name": "email",
-    "buttonText": "Let's do this!",
-    "options": {
-      "attrs": {
-        "placeholder": "Your Email"
-      }
     },
-    "validation": {
+  },
+  {
+    type: "information",
+    label: "",
+    name: "info",
+    text: `being a real-life superhero is not about physical strength, but about <strong>mental strength</strong>.`,
+    buttonText: "Cool, let's do this!",
+    options: {
+      attrs: {},
+    },
+    validation: {
+      required: false,
+    },
+  },
+  {
+    type: "radio",
+    label: "Have you identified your superpowers?",
+    name: "hasSuperpowers",
+    // buttonText: "What's next",
+    options: {
+      choices: ["Yes", "No", "Maybe"],
+    },
+    validation: {
       required: true,
-      email: true
-    }
+    },
   },
   {
-    "type": "text",
-    "label": "Your phone Number",
-    "name": "phone",
-    "buttonText": "Okay. Next!",
-    "options": {
-      "attrs": {
-        "placeholder": "Your Contact"
-      }
+    type: "checkbox",
+    label: "Select applicable superpowers.",
+    name: "superpowers",
+    buttonText: "Okay, cool!",
+    options: {
+      choices: [
+        "Creative",
+        "Analytical",
+        "Logical",
+        "Strategic",
+        "Problem solver",
+      ],
     },
-    "validation": {
+    validation: {
       required: true,
-      numeric: true
-    }
+      minLength: 2,
+    },
   },
   {
-    "type": "textarea",
-    "label": "Tell us why do you want to study with us?",
-    "name": "message",
-    "buttonText": "I'm done now.",
-    "options": {
-      "attrs": {
-        "placeholder": "Write here..."
-      }
+    type: "textarea",
+    label: "Tell us a little bit about your kryptonite",
+    name: "weakness",
+    buttonText: "Next...",
+    options: {
+      attrs: {
+        placeholder: "Write here...",
+      },
     },
-    "validation": {
-      required: false
-    }
+    validation: {
+      required: false,
+    },
   },
-]
+  {
+    type: "text",
+    label: "Describe yourself in one word!",
+    name: "oneWord",
+    buttonText: "Next...",
+    options: {
+      attrs: {
+        placeholder: "Persistent",
+      },
+    },
+    validation: {
+      required: true,
+      minLength: 5,
+    },
+  },
+  {
+    type: "text",
+    label: "Have you thought of your superhero name?",
+    name: "superheroName",
+    buttonText: "What do we call you?",
+    options: {
+      attrs: {
+        placeholder: "Superhero name",
+      },
+    },
+    validation: {
+      required: false,
+      minLength: 5,
+    },
+  },
+  {
+    type: "information",
+    label: "",
+    name: "info",
+    text: `If you are the right fit for being a superhero, tell us how can we reach you?`,
+    buttonText: "Sure...",
+    options: {
+      attrs: {},
+    },
+    validation: {
+      required: false,
+    },
+  },
+  {
+    type: "text",
+    label: "Your Email",
+    name: "email",
+    // buttonText: "Let's do this!",
+    options: {
+      attrs: {
+        placeholder: "Your Email",
+      },
+    },
+    validation: {
+      required: true,
+      email: true,
+    },
+  },
+  {
+    type: "text",
+    label: "Your phone Number",
+    name: "phone",
+    buttonText: "Done!",
+    options: {
+      attrs: {
+        placeholder: "Your Contact",
+      },
+    },
+    validation: {
+      required: true,
+      numeric: true,
+    },
+  },
+];
 
 export default {
-  form
-}
+  form,
+};
---
+++ src/config/constants.js
@@ -0,0 +1,3 @@
+export const instructions = `Looking to be a superhero? <p class="instructions"> Use <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="m4.431 12.822l13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/></svg>
+ key to go back and <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"/></svg>
+    key to move forward within the form.</p>`;
